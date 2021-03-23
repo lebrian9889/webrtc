d8cdee09d41dbd458032bc71fe20ce6fba71e85f
brian <brian@local.host>
2021-03-23 13:39:03 +0700

    first commit

---
+++ public/js/config.js
@@ -0,0 +1,16 @@
+iceServers: [
+  { urls: ["stun:ss-turn1.xirsys.com"] },
+  {
+    username:
+      "WhNjcOFEs8KaPz_fcyghFDSXyL_2rnOOS_1-4Hche_k0OwWRUhtrQjz00gD-nnm5AAAAAGBZcRBsZW1pbmg5ODg5",
+    credential: "ca81cb24-8b91-11eb-ab4e-0242ac140004",
+    urls: [
+      "turn:ss-turn1.xirsys.com:80?transport=udp",
+      "turn:ss-turn1.xirsys.com:3478?transport=udp",
+      "turn:ss-turn1.xirsys.com:80?transport=tcp",
+      "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+      "turns:ss-turn1.xirsys.com:443?transport=tcp",
+      "turns:ss-turn1.xirsys.com:5349?transport=tcp",
+    ],
+  },
+];
